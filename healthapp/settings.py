90cdea48429ef7b1552112ecd4b73040a5c8b3e2
clareysean <seanpclarey@gmail.com>
2023-08-15 13:13:29 -0400

    adjust settings.py configs for deploy on render

--- healthapp/settings.py
+++ healthapp/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
