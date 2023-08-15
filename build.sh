e625be19a75656c5ac0c1c07509777e540f331f1
clareysean <seanpclarey@gmail.com>
2023-08-15 13:21:32 -0400

    adjust settings.py configs for deploy on render

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Install dependencies, including gunicorn
+pip install -r requirements.txt
+
+# Run Gunicorn with your Django application
+gunicorn your_project.wsgi:application
+
