13d59d916e4bf574889623cb03d7da8c7ffc34f0
clareysean <seanpclarey@gmail.com>
2023-08-15 14:09:25 -0400

    adjust settings.py configs for deploy on render

--- healthapp/settings.py
+++ healthapp/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
